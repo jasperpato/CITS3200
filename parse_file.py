142ae4e26fe1c0ea9d97201346221d1a58c2d6a0
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-04 14:14:23 +0800

    cleaned up parse_file function a bit: 	- removed weird newline handling stuff, seems to work 	- extracted post parsing into a separate function parse_post 	- extracted post grouping (into threads) into separate function group
    Thread/Post classes now use parameterised constructors

--- parse_file.py
+++ parse_file.py
@@ -3,57 +3,40 @@ import datetime
 from itertools import groupby
 
 class Thread:
-    def __init__(self):
-        self.subject = ""
-        self.posts = []
+    def __init__(self, subject, posts):
+        self.subject = subject
+        self.posts = posts
         
 class Post:
-    def __init__(self):
-        self.date = None    # datetime.datetime object
-        self.subject = ""
-        self.payload = ""
-        self.verified = False
-        
+    def __init__(self, date, subject, payload, verified):
+        self.date = date    # datetime.datetime object
+        self.subject = subject
+        self.payload = payload
+        self.verified = verified
+
+# expand this with other demonstrators
+valid = set(["chris.mcdonlad@uwa.edu.au"])
+
+def parse_post(post_str): 
+    message = email.message_from_string(post_str)
+    header = dict(message)
+
+    date = datetime.datetime.strptime(header["Date"], "%a %b %d %H:%M:%S %Y")
+    subject = header["Subject"]
+    payload = message.get_payload()
+    verified = header["From"] in valid
+
+    return Post(date,subject,payload,verified)
+
+def group(posts):
+    sub = lambda x: x.subject
+    return [Thread(subject, list(posts)) for subject, posts in
+                    groupby(sorted(posts, key = sub), key = sub)]
+
 def parse_file(filename):
-    
-    threads = []
-    
-    with open(filename, 'r') as file: # text file must begin with a new line
-        
+    with open(filename, 'r') as file:
         s = file.read()
-            
-        posts_strings = ["Date: " + x for x in s.split("\nDate: ")][1:]
-        posts = []
+        post_strings = ["Date: " + x for x in s.split("Date: ")][1:]
+        posts = [parse_post(s) for s in post_strings]
         
-        for p in posts_strings:
-            post = Post()
-            
-            message = email.message_from_string(p)
-            post.payload = message.get_payload()
-            
-            header = dict(message)
-            
-            post.date = datetime.datetime.strptime(header["Date"], "%a %b %d %H:%M:%S %Y")
-            post.subject = header["Subject"]
-            
-            post.verified = header["From"] == "chris.mcdonald@uwa.edu.au" # + demonstrators
-            
-            posts.append(post)
-            
-    threads = []
-    
-    for _, post in groupby(sorted(posts, key = lambda x: x.subject), key = lambda x: x.subject):
-        threads.append(list(post))
-    
-    
-    for t in threads:
-        for p in t:
-            print(p.date)
-            print(p.subject)
-    
-    return threads
-        
-
-threads = parse_file("help2002/help2002-2017.txt")
-
-    
+    return group(posts)
