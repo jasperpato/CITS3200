1fbdf6ca3887e3e9a1d70d9ad09208aab41f24cb
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-04 14:30:00 +0800

    Adding summarise.py (from #resources channel)

---
+++ summarise.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from math import sqrt
+from typing import List, Set
+from utils import merge
+
+Vector = List[float]
+Tokens = List[str]
+
+def dot_product(xs : Vector, ys : Vector) -> float:
+    return sum([x*y for (x,y) in zip(xs,ys)])
+
+def norm(xs : Vector) -> float:
+    return sqrt(dot_product(xs,xs))
+
+def word_vector(toks : Tokens, words : Set[str]) -> Vector:
+    return list(merge({w : 0 for w in words}, Counter(toks)).values())
+
+def cosine_similarity(A : Tokens, B : Tokens) -> float:
+    all_words = set(A) | set(B)
+    A_vec = word_vector(A, all_words)
+    B_vec = word_vector(B, all_words)
+    return dot_product(A_vec, B_vec) / (norm(A_vec) * norm(B_vec))
