26e791947652202deed9acb3f821a42fbe5ac440
Stephen Wild (22422481) <22422481@student.uwa.edu.au>
2021-08-10 14:53:23 +0800

    Add type hints 	- parameterised List[Thread], List[Post]

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,11 @@
 from parse_file import *
 from post import Post
 from thread import Thread
+from typing import List
 
 """
 This function takes a new post object and a list of thread objects
 and returns a tuple of the n most similar existing posts to the new post.
 """
-def find_similar_posts(post : Post, threads : list, n : int):
+def find_similar_posts(post : Post, threads : List[Thread], n : int) -> List[Post]:
     return None
