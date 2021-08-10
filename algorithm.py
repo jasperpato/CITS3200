7c90213269cf12bd1e8a2cf7ac9c5440b6dfafc6
Stephen Wild (22422481) <22422481@student.uwa.edu.au>
2021-08-10 15:37:19 +0800

    Cleaned up import syntax

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,7 @@ from post import Post
 from thread import Thread
 from typing import List
 from summarise import cosine_similarity
-import heapq
+from heapq import nlargest
 from itertools import chain
 from tokeniser import preprocess
 
@@ -16,4 +16,4 @@ def find_similar_posts(post : Post, threads : List[Thread], n : int, subject : b
     all_posts = chain(*[t.posts for t in threads])
     k = (lambda x: x.subject) if subject else (lambda x: x.payload)
     post_scores = {p:cosine_similarity(preprocess(k(post)), preprocess(k(p))) for p in all_posts}
-    return tuple(heapq.nlargest(n, post_scores, key=post_scores.get))
+    return tuple(nlargest(n, post_scores, key=post_scores.get))
