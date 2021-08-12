1ce6fbc4de5c26d3e96838ce80094164f92542f7
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-12 15:41:58 +0800

    Change type hint to reflect spec in algorithm.py

--- cosine_algorithm.py
+++ cosine_algorithm.py
@@ -1,12 +1,12 @@
 from post import Post
 from thread import Thread, all_posts
-from typing import List
+from typing import List, Tuple
 from summarise import cosine_similarity
 from heapq import nlargest
 from itertools import chain
 from tokeniser import preprocess
  
-def cosine_algorithm(post : Post, threads : List[Thread], n : int, subject : bool) -> List[Post]:  
+def cosine_algorithm(post : Post, threads : List[Thread], n : int, subject : bool) -> Tuple[Post]:  
     k = (lambda x: x.subject) if subject else (lambda x: x.payload)
     post_scores = {p:cosine_similarity(preprocess(k(post)), preprocess(k(p))) for p in all_posts(threads)}
     return tuple(nlargest(n, post_scores, key=post_scores.get))
