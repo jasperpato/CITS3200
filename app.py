92853bab45571ae65f88e79efcd7a437d06705c8
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-12 16:50:24 +0800

    Radio buttons for picking corpus

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from algorithm import find_similar_posts
 
 app = Flask("Evaluator")
 
-threads = parse_file('help2002-2017.txt')
+threads = []
+for path in ['help2002-2017.txt', 'help2002-2018.txt', 'help2002-2019.txt']:
+    threads.append(parse_file(path))
 
 @app.route("/", methods=["GET", "POST"])
 def main():
@@ -26,6 +28,12 @@ def main():
                 <br>
                 <input type=number name=nposts>
                 <br>
+                <input type="radio" id="2017" name="year" value="0">
+                <label for="2017">help2002-2017.txt</label><br> 
+                <input type="radio" id="2018" name="year" value="1">
+                <label for="2018">help2002-2018.txt</label><br> 
+                <input type="radio" id="2019" name="year" value="2">
+                <label for="2019">help2002-2019.txt</label><br> 
                 <input type=submit value=Yall>
             </form>
         """
@@ -34,4 +42,4 @@ def main():
     else:
         post = Post(None, request.form['subject'], request.form['payload'], None)
         return form + "<ul>" + "".join(["<li>"+str(p).replace('\n', '<br>')+"</li>"
-                    for p in find_similar_posts(post, threads, int(request.form['nposts']), False)]) + "</ul>"
+                    for p in find_similar_posts(post, threads[int(request.form['year'])], int(request.form['nposts']), False)]) + "</ul>"
