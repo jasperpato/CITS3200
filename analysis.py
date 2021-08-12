fb89dfc7d58e78ebaf6e27c6ae060725dd8a2640
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-12 12:43:55 +0800

    Add function for showing words with more than n occurences

--- analysis.py
+++ analysis.py
@@ -19,6 +19,12 @@ def plot_word_freq(threads : List[Thread]):
    plt.hist(freqs, bins=500) # dunno mang
    plt.show()
 
+def words_n_occurences(threads : List[Thread], n : int) -> List[str]:
+    p = [(k,v) for k,v in Counter(chain(*[preprocess(p.payload) for p in all_posts(threads)])).items() if v > 100]
+    p.sort(key=lambda x: x[1], reverse=True)
+    return [k for k,v in p]
+
 # intend to use this file as a CLI tool 
 if __name__ == "__main__":
-    plot_word_freq(parse_file(argv[1]))
+    threads = parse_file(argv[1])
+    print(words_n_occurences(threads, 50)) # words that appear more than 50 times
