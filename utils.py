071e4a6542e0064ecfa45fc56294b75a67eb6ab4
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-12 23:41:09 +0800

    Add pipe function

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
+from functools import reduce
+
 # in python 3.9 we can just 'da | db'
 def merge(da : dict, db : dict) -> dict:
     z = da.copy()
     z.update(db)
     return z
+
+def pipe(*args):
+    return lambda x: reduce(lambda y,z: z(y), args, x)
