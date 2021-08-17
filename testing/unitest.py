02827128d260efe101f8968e27b0770e5ccf629b
kurapikaaaa <petersheng2@outlook.com>
2021-08-17 13:14:16 +0800

    test prototype

--- testing/unitest.py
+++ testing/unitest.py
@@ -1,4 +1,21 @@
 import json 
+import unittest
 
-def test():
+class testConfig():
     pass
+
+class fileParseCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_parse():
+        pass
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(fileParseCase('test_parse'))
+    return suite
+
+if __name__=='__main__':
+    unittest.main(verbosity=2)
