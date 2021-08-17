50817629102c774103620a16592e1878d97890f5
stephenbrodiewild <stephenbrodiewild@gmail.com>
2021-08-17 14:54:26 +0800

    Added stemming and support for stemming via website

--- algorithm.py
+++ algorithm.py
@@ -7,10 +7,9 @@ from utils import pipe, cached
 from summarise import cosine_similarity
 from itertools import product
 
-# payload is piped through substitution functions, then result is tokenised,
-# and resulting tokens are filtered out if they hit any of the given filters
-def process_post(p : Post, substitutes : Tuple[Callable[[str], str]], filters : Tuple[Callable[[str], bool]]):
-        return list(filter(lambda x: any(product(filters,x)), word_tokenize(pipe(*substitutes)(p.payload))))
+# clean and tokenize the text, filter out specified tokens, and then apply substitutions to the tokens
+def process_post(p : Post, cleaners : Tuple[Callable[[str], str]], filters : Tuple[Callable[[str], bool]], substitutes : Tuple[Callable[[str], str]]):
+        return [pipe(*substitutes)(tok) for tok in word_tokenize(pipe(*cleaners)(p.payload)) if any(product(filters,tok))]
 
 # process_post, except memoised so as to avoid recomputation.
 #   Reduces query speed by an order of magnitude in some circumstances.
@@ -18,13 +17,14 @@ process_cached = cached(process_post)
 
 def pipe_line(  post : Post,
                 threads : List[Thread],
-                substitutes : Tuple[Callable[[str], str]],   # functions that apply textual substitution
+                cleaners : Tuple[Callable[[str], str]],
                 filters : Tuple[Callable[[str], bool]],      # functions that check if a string should be filtered out
+                substitutes : Tuple[Callable[[str], str]],   # functions that apply textual substitution
                 weights : List[Callable[[Post], float]],    # functions that return a scalar value that scales a post up/down
                 n : int) -> List[Post]:
 
-    post_toks = process_post(post, substitutes, filters)
+    post_toks = process_post(post,cleaners,filters,substitutes)
 
     # check similarity between given post and all other posts, and then scale with weights
-    post_scores = {p:(pipe(*weights)(p))*(cosine_similarity(post_toks, process_cached(p,substitutes,filters))) for p in all_posts(threads)}
+    post_scores = {p:(pipe(*weights)(p))*(cosine_similarity(post_toks, process_cached(p,cleaners,filters,substitutes))) for p in all_posts(threads)}
     return nlargest(n, post_scores, key=post_scores.get)
--- app.py
+++ app.py
@@ -18,7 +18,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
 
-##################### Substitutes ######################################
+##################### cleaners ######################################
 
 lower = lambda x: x.lower()
 space = lambda x: sub(r'\s+', ' ', x)
@@ -27,6 +27,10 @@ space = lambda x: sub(r'\s+', ' ', x)
 
 not_ascii = lambda x: x not in ascii_letters
 stopword = lambda x: x in stopwords
+
+##################### substitutes ######################################
+stemmer = nltk.stem.snowball.SnowballStemmer("english")
+stem = stemmer.stem
 
 #################### Weights #######################################
 
@@ -66,28 +70,31 @@ page = """
                     <br>
                     <label for="nposts">Number of posts</label>
                     <br>
-                    <input type=number name=nposts min=1>
+                    <input type=number name=nposts min="1" value="5" max="10">
                     <br>
-                    <input type="radio" id="2017" name="year" value="0">
+                    <input type="radio" id="2017" name="year" value="0" checked>
                     <label for="2017">help2002-2017.txt</label><br> 
                     <input type="radio" id="2018" name="year" value="1">
                     <label for="2018">help2002-2018.txt</label><br> 
                     <input type="radio" id="2019" name="year" value="2">
                     <label for="2019">help2002-2019.txt</label><br>
 
-                    <input type="checkbox" id="lower" name="lower" value="False>
+                    <input type="checkbox" id="lower" name="lower" checked>
                     <label for="lower">search with lowercase</label><br> 
 
-                    <input type="checkbox" id="space" name="space" value=False>
+                    <input type="checkbox" id="space" name="space" checked>
                     <label for="space">remove extraneous spaces</label><br> 
 
-                    <input type="checkbox" id="ascii" name="ascii" value=False>
+                    <input type="checkbox" id="ascii" name="ascii" checked>
                     <label for="ascii">limit search to non-ascii</label><br>
 
-                    <input type="checkbox" id="stopwords" name="stopwords" value=False>
+                    <input type="checkbox" id="stopwords" name="stopwords" checked>
                     <label for="stopwords">filter out stopwords</label><br>
 
-                    <input type=number name="weight" min=0.0>
+                    <input type="checkbox" id="stem" name="stem" checked>
+                    <label for="stem">Apply stemming</label><br>
+
+                    <input type=number name="weight" min="1" value="1">
                     <label for="weight">scaling applied to Chris' posts</label><br>
                     <input type=submit value=Yall>
                 </form>
@@ -101,21 +108,24 @@ def search():
     threads = all_threads[int(request.args['year'])]
     
     nposts = int(request.args['nposts'])
-    substitutes = []
-    if('lower' in request.args): substitutes.append(lower)
-    if('space' in request.args): substitutes.append(space)
+    cleaners = []
+    if('lower' in request.args): cleaners.append(lower)
+    if('space' in request.args): cleaners.append(space)
     
     filters = []
     if('ascii' in request.args): filters.append(not_ascii)
     if('stopwords' in request.args): filters.append(stopword)
 
+    substitutes = []
+    if('stem' in request.args): substitutes.append(stem)
+
     weights = []
     weights.append(verified(float(request.args['weight'])))
     
-    posts = pipe_line(post, threads, tuple(substitutes), tuple(filters), weights, nposts)
+    posts = pipe_line(post, threads, tuple(cleaners), tuple(filters), tuple(substitutes), weights, nposts)
     
     time_taken = time.time() - time_start
-    return page + f"<article><h3>{time_taken} seconds</h3><br><ul>" + "".join(["<li>{0}</li><br>".format(str(p).replace('\n', '<br>')) for p in posts])+ "</ul>"
+    return page + f"<article><h3>{time_taken} seconds</h3><br><h2>Subject: {request.args['subject']}<br>Payload: {request.args['payload']}</h2><ul>" + "".join(["<li>{0}</li><br>".format(str(p).replace('\n', '<br>')) for p in posts])+ "</ul>"
 
 @app.route("/")
 def main():
