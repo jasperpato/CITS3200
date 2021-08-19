6d84b25b26c8989cc04696fd5de3f51d97dbfae0
JasonWML <jason.technic@gmail.com>
2021-08-19 01:11:01 -0700

    Revised functions to suite branch update

--- cosine_algorithm.py
+++ cosine_algorithm.py
@@ -5,9 +5,8 @@ from summarise import cosine_similarity
 from heapq import nlargest
 from itertools import chain
 from tokeniser import preprocess
-from weights import weight
 
 def cosine_algorithm(post : Post, threads : List[Thread], n : int, subject : bool) -> Tuple[Post]:
     k = (lambda x: x.subject) if subject else (lambda x: x.payload)
-    post_scores = {p:cosine_similarity(preprocess(k(post)), preprocess(k(p)))*weight(p)for p in all_posts(threads)}
+    post_scores = {p:cosine_similarity(preprocess(k(post)), preprocess(k(p)))for p in all_posts(threads)}
     return tuple(nlargest(n, post_scores, key=post_scores.get))
