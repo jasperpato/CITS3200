41f18c7c2817dd26dc6ccf37f737bd20869a1570
Jasper Paterson <jasperpaterson@Jaspers-MacBook-Pro.local>
2021-08-22 22:03:48 +0800

    Implement jaccard similarity in algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -20,3 +20,16 @@ def cosine_similarity(A : Tokens, B : Tokens) -> float:
     A_vec = word_vector(A, all_words)
     B_vec = word_vector(B, all_words)
     return dot_product(A_vec, B_vec) / (norm(A_vec) * norm(B_vec))
+
+# calculates the intersection / union of two lists of tokens
+# assumes there are no duplicates within A or B
+def jaccard(A : Tokens, B : Tokens) -> float:
+    union = len(A)
+    intersection = 0
+    for s in B:
+        if s not in A:
+            union += 1
+        else:
+            intersection += 1
+    return intersection / union
+    
