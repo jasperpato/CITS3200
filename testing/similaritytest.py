c255998bef47bac2491bf341852cef1a9911bce7
kurapikaaaa <petersheng2@outlook.com>
2021-08-24 15:58:24 +0800

    pipeline test prototype

--- testing/similaritytest.py
+++ testing/similaritytest.py
@@ -1,16 +1,31 @@
 from pipeline import process_post, pipeline
 import json
 from utils import reduce
-import Thread
-import Post
+from thread import Thread
+from post import Post
+from string import ascii_letters
+import nltk
+from re import sub
+stopwords = nltk.corpus.stopwords.words('english')
 
-def test(self):
+def test():
     space = open("test_space_2019.json")
     case = open("test_case_2019.json")
     threads = json.load(space)
     tests = json.load(case)
-    for test_case in tests:
-        top = pipeline(parse_thread(threads), parse_post(test_case), 3, False)
+    substitutes = ( lambda x: x.lower(),                # lowercase all text
+                    lambda x: sub(r'\s+', ' ', x))      # remove extraneous spaces
+
+    filters = (  lambda x: x not in ascii_letters,       # take non-alphabetical words out
+                lambda x: x in stopwords)               # remove stopwords
+
+    weights = [ lambda x: 1.5 if x.verified else 1.0]   # give a bit more priority to Chris' posts
+    cleaner = [lambda x:x]
+
+    for test_case in tests["testcases"]:
+        print(test_case['Subject'])
+        print('\n')
+        top = pipeline(parse_post(dict(test_case)),parse_thread(threads),cleaner, substitutes, filters, weights, 3)
         for closest in top:
             tag_of_Post = next((t for t in threads if closest.payload==t.Body).split(" "), None)
             print(tag_of_Post)
@@ -18,9 +33,10 @@ def test(self):
     pass
 
 def parse_post(post):
-    return Post(post.Date, post.Subject, post.body,True)
+    return Post("Wed Oct  9 17:02:25 2019", post['Subject'], post['Body'],True)
 
 def parse_thread(thread):
-    posts = [parse_post(this_post) for this_post in thread]
-    return(Thread(this_post.Subject, posts) for this_post in thread)
+    posts = [parse_post(this_post) for this_post in thread['testcases']]
+    return(Thread(this_post['Subject'], posts) for this_post in thread['testcases'])
    
+test()
