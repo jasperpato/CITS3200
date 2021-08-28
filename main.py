662c546e41382eda0f5471a9d7d6c68ed402e47b
Jasper Paterson <jasperpaterson@Jaspers-MacBook-Pro.local>
2021-08-28 14:09:57 +0800

    match on subject with weight w, untested

--- main.py
+++ main.py
@@ -19,17 +19,19 @@ if __name__=="__main__":
     new_post.subject = input("Subject: ")
     new_post.payload = input("Question body: ")
     N = 3 # number of most similar posts
+    W = 0.3 # weight of subject similarity, payload weight is (1.0 - W)
 
-    filters = (  lambda x: x not in ascii_letters,       # take non-alphabetical words out
-                lambda x: x in stopwords)               # remove stopwords
+    filters = (  lambda x: x not in ascii_letters, # take non-alphabetical words out
+                lambda x: x in stopwords)          # remove stopwords
 
-    weights = [ lambda x: 1.5 if x.verified else 1.0]   # give a bit more priority to Chris' posts
+    weights = [ lambda x: 1.5 if x.verified else 1.0] # gives priority to Chris' posts
 
-    cleaners =    ( lambda x: x.lower(),                # lowercase all text
+    cleaners =    ( lambda x: x.lower(),              # lowercase all text
                     lambda x: sub(r'\s+', ' ', x))
 
     substitutes = tuple([])
 
-    algos = (cosine_similarity, jaccard)
-    for p in pipeline(new_post, threads, cleaners, filters, substitutes,  weights, algos, N):
+    algorithms = (cosine_similarity, jaccard)
+
+    for p in pipeline(new_post, threads, cleaners, filters, substitutes,  weights, algorithms, W, N):
         print(p)
