bd4717ced753e9b5af791c1da6a548340d28fd74
Allen Antony <22706998@student.uwa.edu.au>
2021-08-31 03:06:45 +0800

    Refactorisation

--- USE.py
+++ USE.py
@@ -1,24 +1,10 @@
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow.compat.v1 as tf_v1
+tf_v1.disable_v2_behavior()
 import tensorflow_hub as hub
-
-'''
-Versions of packages 
-tensorflow==2.0.0
-tensorflow-estimator==2.0.1
-tensorflow-hub==0.7.0
-'''
 
 import numpy as np
 import pandas as pd
-from typing import List, Tuple
 from heapq import nlargest
-
-'''
-Versions of packages
-numpy==1.17.2
-pandas==0.25.1
-'''
 
 from post import Post
 from thread import Thread, all_posts
@@ -26,15 +12,12 @@ from thread import Thread, all_posts
 
 # load universal sentence encoder module
 def load_USE_encoder(module):
-    with tf.Graph().as_default():
-        sentences = tf.placeholder(tf.string)
+    with tf_v1.Graph().as_default():
+        sentences = tf_v1.placeholder(tf_v1.string)
         embed = hub.Module(module)
         embeddings = embed(sentences)
-        session = tf.train.MonitoredSession()
+        session = tf_v1.train.MonitoredSession()
     return lambda x: session.run(embeddings, {sentences: x})
-
-
-encoder = load_USE_encoder('../USE_model')
 
 
 def cosine_similarity(vec1, vec2):
@@ -46,8 +29,7 @@ def cosine_similarity(vec1, vec2):
 
 # return a pandas dataframe that includes the similarity between every post. 
 # can be used to generate clusters
-def get_similarity_dataframe(threads):
-    posts = all_posts(threads)
+def get_similarity_dataframe(posts, encoder):
     k = lambda x: x.subject if x.subject else lambda x: x.payload
     encoded_posts = encoder([k(post) for post in posts])
     num_posts = len(posts)
@@ -67,10 +49,17 @@ def get_similarity_dataframe(threads):
 
 
 # Identical in use-case to the function defined in algorithm.py
-def find_similar_posts(post, threads, n, subject):
-    posts = all_posts(threads)
+def find_similar_posts(post, n, encoded_posts, encoder):
     k = lambda x: x.subject if x.subject else lambda x: x.payload
-    encoded_posts = encoder([k(post) for post in posts])
     in_vec = encoder([k(post)])[0]
     post_scores = {p: cosine_similarity(in_vec, p) for p in encoded_posts}
     return tuple(nlargest(n, post_scores, key=post_scores.get))
+
+
+def encode_posts(posts, save_name):
+    encoder = load_USE_encoder('USE')
+    k = lambda x: x.subject if x.subject else lambda x: x.payload
+    encoded_posts = encoder([k(post) for post in posts])
+    np.save(f'./encodings/{save_name}')
+    return encoded_posts
+    
