9c87a4510bbedf422cccce09b126b780ef6501c3
Allen Antony <22706998@student.uwa.edu.au>
2021-09-07 02:56:38 +0800

    Added tfidf similarity function

--- tfidf.py
+++ tfidf.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from heapq import nlargest
+import json
 
 
 def save_tfidf_embeddings(posts, filename):
@@ -16,7 +18,17 @@ def add_encoding(post, npfile):
 	np.save(f'../encodings/tfidf/{npfile}', np.append(embeddings, new_vec))
 
 
-def tfidf_similarity(post, posts):
+def tfidf_similarity(post, embeddings, posts, n):
 	vectorizer = TfidfVectorizer()
 	vec = vectorizer.fit_transform([post.payload])
+	scores = cosine_similarity(vec, embeddings).flatten()
+	post_scores = {posts[i]:scores[i] for i in range(len(posts))}
+	return nlargest(n, post_scores, key=post_scores.get)
+
+
+if __name__ == '__main__':
+	from testing.similaritytest import parse_post
+	test_space = json.load(open('./testing/test_space_2019.json'))['testcases']
+	posts = [parse_post(p) for p in test_space]
+	save_tfidf_embeddings(posts, 'test_space.npy')
 	
