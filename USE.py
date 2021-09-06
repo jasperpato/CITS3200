4f858cdea36240ba44f1b50b5df28cd89e85d3c1
Allen Antony <22706998@student.uwa.edu.au>
2021-09-06 23:11:49 +0800

    Refactoring

--- USE.py
+++ USE.py
@@ -33,9 +33,7 @@ def cosine_similarity(vec1, vec2):
 
 # return a pandas dataframe that includes the similarity between every post. 
 # can be used to generate clusters
-def get_similarity_dataframe(posts, encoder):
-    k = lambda x: x.subject if x.subject else lambda x: x.payload
-    encoded_posts = encoder([k(post) for post in posts])
+def get_similarity_dataframe(posts, encoded_posts, encoder):
     num_posts = len(posts)
     similarities_df = pd.DataFrame()
     for i in range(num_posts):
@@ -54,8 +52,7 @@ def get_similarity_dataframe(posts, encoder):
 
 # Identical in use-case to the function defined in algorithm.py
 def similarity_function_USE(post, encoded_posts, posts, n, encoder):
-    k = lambda x: x.subject if x.subject else lambda x: x.payload
-    in_vec = encoder([k(post)])[0]
+    in_vec = encoder([post.payload])[0]
     scores = [cosine_similarity(in_vec, p) for p in encoded_posts]
     post_dict = {posts[i]:scores[i] for i in range(len(posts))}
     return tuple(nlargest(n, post_dict, key=post_dict.get))
@@ -69,10 +66,11 @@ def encode_posts(posts, save_name):
 
 
 if __name__== '__main__':
-    #test_space_posts = json.load(open("testing/test_space_2019.json"))["testcases"]
-    #posts = [parse_post(p) for p in test_space_posts]
-    #encode_posts(posts, 'test_space.npy')
-    pass
+    from testing.similaritytest2 import parse_post
+
+    test_space_posts = json.load(open("testing/test_space_2019.json"))["testcases"]
+    posts = [parse_post(p) for p in test_space_posts]
+    encode_posts(posts, 'test_space.npy')
 
 
     
