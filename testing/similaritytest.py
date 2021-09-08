f2e0c85cb60818a45ff9f6a6d02d0b5a7460140f
Allen Antony <22706998@student.uwa.edu.au>
2021-09-09 03:11:47 +0800

    Added testing functionality for bert

--- testing/similaritytest.py
+++ testing/similaritytest.py
@@ -12,60 +12,25 @@ from pipeline import process_post, pipeline
 from parse_file import valid, group_into_threads
 from algorithms import cosine_similarity, jaccard
 from tfidf import tfidf_similarity
-from USE import similarity_function_USE
+from use import use_similarity, load_use_model
+from bert import bert_similarity, load_bert_model
 stopwords = nltk.corpus.stopwords.words('english')
 
-
-def pipeline_test():
-    test_space_posts = json.load(open("testing/test_space_2019.json"))["testcases"]
-    test_case_posts = json.load(open("testing/test_case_2019.json"))["testcases"]
-    cleaner =  (lambda x: x.lower(),                # lowercase all text
+cleaner =  (lambda x: x.lower(),                # lowercase all text
                     lambda x: sub(r'\s+', ' ', x))      # remove extraneous spaces and punctuation
 
-    filters =  (lambda x: x not in ascii_letters,       # take non-alphabetical words out
-                lambda x: x in stopwords)               # remove stopwords
+filters =  (lambda x: x not in ascii_letters,       # take non-alphabetical words out
+            lambda x: x in stopwords)               # remove stopwords
 
-    weights = (verified_weight,date_weight)
-    #[lambda x: 1.5 if x.verified else 1.0]    # give a bit more priority to Chris's posts
-    substitutes = tuple([])
-    alg = (cosine_similarity, jaccard)
-    total_sum = 0
-    for i, test_case in enumerate(test_case_posts):
-        print(f"{i + 1}'th Test Case")
-        print(f"Subject: {test_case['Subject']}\n")
-        print(f"body: {test_case['Body']}\n")
-        print("________________________")
-        ###
-        top_posts = pipeline(parse_test_case(test_case), parse_test_space(test_space_posts), cleaner, filters, substitutes, weights, alg, test_case["target_count"])
-        ###
-        target_categories = set(test_case['Category'].split(' '))
-        sum = 0
-        for j, post in enumerate(top_posts):
-            test_space_post = [tp for tp in test_space_posts if tp['Body'] == post.payload][0]
-            post_categories = set(test_space_post['Category'].split(' '))
-            sum = sum+1 if len(target_categories.intersection(post_categories)) > 0 else sum
-            #score = len(target_categories.intersection(post_categories)) / len(target_categories)
-            print(f"\t{j + 1}'th Post:") 
-            #print(f"Evaluation score: {score}")
-            print(f"post category: {post_categories}\n")
-            print(f"target category: {target_categories}\n")
-            print(f"{post}")
-            print("________________________")
-        total_sum += sum / test_case["target_count"]
-        print(f'Total case identified:{sum}\n')
-        print(f'Target existing:{test_case["target_count"]}\n')
-        print(f'Recall: {sum / test_case["target_count"]}\n')
-        print("<<=====================================>>")
-    print('FINAL SCORE:', total_sum / len(test_case_posts))
-    print("<<=====================================>>\n")
+weights = (verified_weight, date_weight)
+#[lambda x: 1.5 if x.verified else 1.0]    # give a bit more priority to Chris's posts
+substitutes = tuple([])
 
 
-def encoder_test():
-    print('\n\n')
+def pipeline_test(algo):
     test_space_posts = json.load(open("testing/test_space_2019.json"))["testcases"]
-    test_case_posts = json.load(open("testing/test_case_2019.json"))["testcases"]  
-    encoded_posts = np.load(join(dirname(__file__), '../../encodings/use/test_space.npy'))
-    posts = [parse_post(post) for post in test_space_posts]
+    test_case_posts = json.load(open("testing/test_case_2019.json"))["testcases"]
+    posts = [parse_post(p) for p in test_space_posts]
     total_sum = 0
     for i, test_case in enumerate(test_case_posts):
         print(f"{i + 1}'th Test Case")
@@ -73,7 +38,20 @@ def encoder_test():
         print(f"body: {test_case['Body']}\n")
         print("________________________")
         ###
-        top_posts = similarity_function_USE(parse_test_case(test_case), encoded_posts, posts, test_case['target_count'], encoder)
+        if algo == 'basic':
+            alg = (cosine_similarity, jaccard)
+            top_posts = pipeline(parse_test_case(test_case), parse_test_space(test_space_posts), 
+                cleaner, filters, substitutes, weights, alg, test_case["target_count"])
+        elif algo == 'tfidf':
+            top_posts = tfidf_similarity(parse_test_case(test_case), posts, test_case['target_count'])
+        elif algo == 'use':
+            load_use_model()
+            encoded_posts = np.load(join(dirname(__file__), '../../encodings/use/test_space.npy'))
+            top_posts = use_similarity(parse_test_case(test_case), encoded_posts, posts, test_case['target_count'])
+        elif algo == 'bert':
+            load_bert_model()
+            encoded_posts = np.load(join(dirname(__file__), '../../encodings/bert/test_space.npy'))
+            top_posts = bert_similarity(parse_test_case(test_case), encoded_posts, posts, test_case['target_count'])
         ###
         target_categories = set(test_case['Category'].split(' '))
         sum = 0
@@ -114,4 +92,4 @@ def parse_test_space(test_space_posts):
 
 if __name__ == '__main__':   
     #pipeline_test()
-    encoder_test()
+    pipeline_test('bert')
