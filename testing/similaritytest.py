03f390ba8c381c447faec21f9fa98125ffc4549f
Allen Antony <22706998@student.uwa.edu.au>
2021-09-10 19:21:32 +0800

    Refactoring functions to increase visibility. Also added a time measurement for the algorithms

--- testing/similaritytest.py
+++ testing/similaritytest.py
@@ -1,13 +1,16 @@
 import json
 import datetime
-from os.path import dirname, join
 import numpy as np
+import time
+import nltk
+import os
+from re import sub
+from os.path import dirname, join
 
 from post import Post
+from thread_obj import all_posts
 from string import ascii_letters
 from weights import date_weight, verified_weight
-import nltk
-from re import sub
 from pipeline import process_post, pipeline
 from parse_file import valid, group_into_threads
 from algorithms import cosine_similarity, jaccard
@@ -31,32 +34,21 @@ substitutes = tuple([])
 def pipeline_test(algo):
     test_space_posts = json.load(open("testing/test_space_2019.json"))["testcases"]
     test_case_posts = json.load(open("testing/test_case_2019.json"))["testcases"]
-    posts = [parse_post(p) for p in test_space_posts]
-    total_sum = 0
+    score_sum = 0
+    time_sum = 0
     for i, test_case in enumerate(test_case_posts):
         print(f"{i + 1}'th Test Case")
         print(f"Subject: {test_case['Subject']}\n")
         print(f"body: {test_case['Body']}\n")
         print("________________________")
         ###
-        if algo == 'basic':
-            alg = (cosine_similarity, jaccard)
-            top_posts = pipeline(parse_test_case(test_case), parse_test_space(test_space_posts), 
-                cleaner, filters, substitutes, weights, alg, test_case["target_count"])
-        elif algo == 'tfidf':
-            top_posts = tfidf_similarity(parse_test_case(test_case), posts, test_case['target_count'])
-        elif algo == 'use':
-            load_use_model()
-            encoded_posts = np.load(join(dirname(__file__), '../../encodings/use/test_space.npy'))
-            top_posts = use_similarity(parse_test_case(test_case), encoded_posts, posts, test_case['target_count'])
-        elif algo == 'bert':
-            load_bert_model()
-            encoded_posts = np.load(join(dirname(__file__), '../../encodings/bert/test_space.npy'))
-            top_posts = bert_similarity(parse_test_case(test_case), encoded_posts, posts, test_case['target_count'])
+        algo_result = evaluate_algo(algo, test_case, test_space_posts, test_case['target_count'])
+        print(f"Time taken: {algo_result['time']}\n")
+        print("________________________")
         ###
         target_categories = set(test_case['Category'].split(' '))
         sum = 0
-        for j, post in enumerate(top_posts):
+        for j, post in enumerate(algo_result['top_posts']):
             test_space_post = [tp for tp in test_space_posts if tp['Body'] == post.payload][0]
             post_categories = set(test_space_post['Category'].split(' '))
             sum = sum+1 if len(target_categories.intersection(post_categories)) > 0 else sum
@@ -67,13 +59,38 @@ def pipeline_test(algo):
             print(f"target category: {target_categories}\n")
             print(f"{post}")
             print("________________________")
-        total_sum += sum / test_case["target_count"]
-        print(f'Total case identified:{sum}\n')
-        print(f'Target existing:{test_case["target_count"]}\n')
+        score_sum += sum / test_case["target_count"]
+        time_sum += algo_result['time']
+        print(f'Total matches identified:{sum}\n')
+        print(f'Number of targets existing:{test_case["target_count"]}\n')
         print(f'Recall: {sum / test_case["target_count"]}\n')
         print("<<=====================================>>")
-    print('FINAL SCORE:', total_sum / len(test_case_posts))
+    print('FINAL SCORE:', score_sum / len(test_case_posts))
+    print('AVERAGE_ALGO_TIME:', time_sum / len(test_case_posts))
     print("<<=====================================>>\n")
+
+
+def evaluate_algo(algo, test_case, test_space_posts, n):
+    if algo == 'use':
+        load_use_model(use_cpu=True)
+    elif algo == 'bert':
+        load_bert_model(use_cpu=True)
+
+    start_time = time.process_time()
+    posts = [parse_post(p) for p in test_space_posts]
+    post = parse_test_case(test_case)
+    if algo == 'basic':
+        algs = (cosine_similarity, jaccard)
+        top_posts = pipeline(post, parse_test_space(posts), cleaner, filters, substitutes, weights, algs, n)
+    elif algo == 'tfidf':
+        top_posts = tfidf_similarity(post, posts, n)
+    elif algo == 'use':
+        encoded_posts = np.load(join(dirname(__file__), '../../encodings/use/test_space.npy'))
+        top_posts = use_similarity(post, encoded_posts, posts, n)
+    elif algo == 'bert':
+        encoded_posts = np.load(join(dirname(__file__), '../../encodings/bert/test_space.npy'))
+        top_posts = bert_similarity(post, encoded_posts, posts, n)
+    return {'top_posts': top_posts, 'time': time.process_time() - start_time}
 
 
 def parse_test_case(post):
@@ -92,4 +109,4 @@ def parse_test_space(test_space_posts):
 
 
 if __name__ == '__main__':   
-    pipeline_test('bert')
+    pipeline_test('use')
