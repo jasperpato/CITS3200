06dda22dab9b4c76853dc6da69f68d12159701a7
GeorgeSandwich <willgd@outlook.com.au>
2021-09-15 00:39:55 +0800

    tweaked clustering.py

--- clustering.py
+++ clustering.py
@@ -20,6 +20,8 @@ def clustering(threads : List[Thread], subject : bool):
         print("post: ", i)
         i = i + 1
 
+        #if i == 10: break
+
         for other_post in all_posts(threads):
 
             #doesn't find how close the thread is to itself
@@ -30,15 +32,17 @@ def clustering(threads : List[Thread], subject : bool):
             cosine_sim = cosine_similarity(preprocess(k(post)), preprocess(k(other_post)))
 
             #sets current closest thread dependending on the cosine similarity
-            if post in closest_vector:
+            if post not in closest_vector:
 
-                if cosine_sim < closest_vector[post][1]:
+                #previous 0.003
+                closest_vector[post] = (None, 0.002)
 
-                    closest_vector[post] = (other_post, cosine_sim)
+            if cosine_sim < closest_vector[post][1]:
 
-            else:
-                
+                if cosine_sim == 0.0: continue
+
                 closest_vector[post] = (other_post, cosine_sim)
+                #print(cosine_sim)
 
     clusters = {}
     
@@ -49,6 +53,8 @@ def clustering(threads : List[Thread], subject : bool):
     #structures the results into a dictionary with the thread as the key and a list of closest threads as the value
     for post in closest_vector:
 
+        if closest_vector[post][0] == None: continue
+        
         clusters[closest_vector[post][0]].append(post)
 
     return clusters
@@ -77,17 +83,17 @@ def main():
             frequent = len(clusters[key])
             frequent_key = key
 
-    print(key.payload)
+    print(frequent_key.payload)
 
     print()
     print("-----------------------------------------------------------")
     print()
 
-    for post in clusters[key]:
+    print(frequent)
+
+    for post in clusters[frequent_key]:
 
         print(post.payload)
         print("----------------")
 
     keys = list(clusters.keys())
-
-    print(keys[0].payload)
