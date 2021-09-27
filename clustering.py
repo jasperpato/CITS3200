7e627549f25bbb4b0368ea7cac2c28c3e8de03d4
GeorgeSandwich <willgd@outlook.com.au>
2021-09-28 01:13:54 +0800

    fixed bugs in verified_clustering function

--- clustering.py
+++ clustering.py
@@ -1,15 +1,25 @@
-from summarise import cosine_similarity
-from thread import Thread, all_posts
-from tokeniser import preprocess
+from algorithms import cosine_similarity
+from thread_obj import Thread, all_posts
+from weights import verified_weight
+from utils import cached, remove_none_alphabet, remove_stopwords, to_lower
 from typing import List
+from pipeline import process_post
+from re import sub
+import time
 
-from parse_file import parse_post, group
+from parse_file import parse_post, group_into_threads
 
 #takes in threads in whatever form they're in (need to find out what that is exactly)
 #creates a dictionary containing a list of each thread closest to the key thread
-def clustering(threads : List[Thread], subject : bool):
+process_cached = cached(process_post)
 
-    k = (lambda x: x.subject) if subject else (lambda x: x.payload)
+cleaners =    ( to_lower,              # lowercase all text
+                    lambda x: sub(r'\s+', ' ', x))
+
+filters = (  remove_none_alphabet, # take non-alphabetical words out
+                remove_stopwords)          # remove stopwords
+
+def clustering(threads : List[Thread], subject : bool):
 
     closest_vector = {}
 
@@ -31,7 +41,8 @@ def clustering(threads : List[Thread], subject : bool):
                 continue
 
             #finds the cosine similarity for each pair of threads
-            cosine_sim = cosine_similarity(preprocess(k(post)), preprocess(k(other_post)))
+            payload_toks = process_post(post, cleaners, filters, tuple([]), False)
+            cosine_sim = cosine_similarity(payload_toks, process_cached(other_post, cleaners, filters, tuple([]), False))
 
             #sets current closest thread dependending on the cosine similarity
             if post not in closest_vector:
@@ -54,7 +65,7 @@ def clustering(threads : List[Thread], subject : bool):
 
                 if cosine_sim == 0.0: continue
 
-                closest_vector[post] = (post, cosine_sim)
+                closest_vector[other_post] = (post, cosine_sim)
 
         all_posts_reduced.remove(post)
         
@@ -76,7 +87,6 @@ def clustering(threads : List[Thread], subject : bool):
 from weights import verified_weight
 
 def verified_clustering(threads : List[Thread], subject : bool):
-    k = (lambda x: x.subject) if subject else (lambda x: x.payload)
     closest_vector = {}
     verified_all_posts = []
     for post in all_posts(threads):
@@ -86,12 +96,13 @@ def verified_clustering(threads : List[Thread], subject : bool):
     all_posts_reduced = all_posts(threads)
     print(len(verified_all_posts))
     for post in verified_all_posts:
-        print("post: ", i)
+        #print("post: ", i)
         i = i + 1
         for other_post in all_posts_reduced:
             if post == other_post:
                 continue
-            cosine_sim = cosine_similarity(preprocess(k(post)), preprocess(k(other_post)))
+            payload_toks = process_post(post, cleaners, filters, tuple([]), False)
+            cosine_sim = cosine_similarity(payload_toks, process_cached(other_post, cleaners, filters, tuple([]), False))
             if post not in closest_vector:
                 closest_vector[post] = (None, 0.002)
             if other_post not in closest_vector:
@@ -101,10 +112,10 @@ def verified_clustering(threads : List[Thread], subject : bool):
                 closest_vector[post] = (other_post, cosine_sim)
             if cosine_sim < closest_vector[other_post][1]:
                 if cosine_sim == 0.0: continue
-                closest_vector[post] = (post, cosine_sim)
+                closest_vector[other_post] = (post, cosine_sim)
         all_posts_reduced.remove(post)
     clusters = {}
-    for post in verified_all_posts:
+    for post in closest_vector:
         clusters[post] = []
     for post in closest_vector:
         if closest_vector[post][0] == None: continue
@@ -119,13 +130,17 @@ def parse_file(filename):
         post_strings = ["Date: " + x for x in s.split("Date: ")][1:]
         posts = [parse_post(s) for s in post_strings]
         
-    return group(posts)
+    return group_into_threads(posts)
 
 def main():
 
     threads = parse_file("help2002-2019.txt")
 
+    start = time.time()
     clusters = verified_clustering(threads, False)
+    end = time.time()
+
+    print(end - start)
 
     frequent = 0
     frequent_key = ""
