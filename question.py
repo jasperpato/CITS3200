73f737a5b2daaf11330bba792d067a5d71ffe60a
kurapikaaaa <petersheng2@outlook.com>
2021-09-30 14:34:36 +0800

    question generator debug

--- question.py
+++ question.py
@@ -22,9 +22,8 @@ def encapsulate(test_cases):
     threads = all_threads[2]
     for case in test_cases:
         post = Post(case['Date'], case['Subject'], case['Body'], False)
-        print(post.payload)
         list_of_tup.append(tuple(generate_post(post, threads, cleaners, filters, substitutes, weight, 5)))
-    return list_of_tup
+    return list_of_tup, test_cases
 
 def generate_post(post, threads, cleaners, filters, substitutes, weights, nposts):
     diff_posts = []
@@ -34,22 +33,38 @@ def generate_post(post, threads, cleaners, filters, substitutes, weights, nposts
     Bag_of_words_posts = pipeline(post, threads, tuple(cleaners), tuple(filters), tuple(substitutes), weights, [bag_of_words],0.2, nposts)
     Tfidf_posts = pipeline(post, threads, tuple(cleaners), tuple(filters), tuple(substitutes), weights, [tf.similarity],0.2, nposts)
     #Use_posts = pipeline(post, threads, tuple(cleaners), tuple(filters), tuple(substitutes), weights, [Us.similarity],0.2, nposts)
-    print(Jaccard_posts[0].payload)
-    #Mod_posts = pipeline(post, threads, tuple(cleaners), tuple(filters), tuple(substitutes), weights, cosine_similarity, nposts)
-    list_of_posts = [Bag_of_words_posts, Jaccard_posts, Tfidf_posts, Use_posts]
-    diff_posts.append(Jaccard_posts[0])
+    list_of_posts = [Bag_of_words_posts, Jaccard_posts, Tfidf_posts]
     diff_posts = find_diff_posts(list_of_posts, diff_posts)
     return diff_posts
 
 def find_diff_posts(list, diff: list):
     for item in list:
         for cos in item:
-                if cos in diff or cos == item[-1]:
-                    diff.append(cos)
-                    break
-                else:
-                    continue
+            select = 0
+            for it in list:
+                if it is not item and cos not in it:
+                    select += 1
+            if select == 2 or cos == item[-1]:
+                diff.append(cos)
+                break
+
     return diff
 
 test = json.load(open("./testing/test_case_2019.json"))["testcases"]
-result = encapsulate(test)
+result, cases = encapsulate(test)
+
+my_dict ={"cosine":0, "jacard":1, "tfidf":2}
+
+key_list = list(my_dict.keys())
+val_list = list(my_dict.values())
+for i in range (len(result)):
+    print("Orig post:", cases[i])
+    for j in range (len(result[i])):
+        print("<<<<---------------------------------->>>>")
+        print("|                                         |")
+        print("|                                         |")
+        print("result {use}: {res}".format(use = key_list[val_list.index(j)],res = result[i][j].payload))
+        print("|                                         |")
+        print("|                                         |")
+        print("<<<<---------------------------------->>>>")
+    print("------------------------------------------------------------------------------------\n")
