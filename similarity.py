49559ffdb51df3fad0b5ef69069421668301d0c5
Jasper Paterson <jasperpaterson@Jaspers-MacBook-Pro.local>
2021-10-14 13:43:00 +0800

    all algos classed

--- similarity.py
+++ similarity.py
@@ -8,7 +8,6 @@ Then it wil lreturn 3 of the most similiar post.
 """
 import sys
 from parse_file import parse_file
-from similarity_algorithms.cosine import cosine_similarity
 from thread_obj import Thread
 from post import Post
 from pipeline import pipeline
@@ -16,10 +15,13 @@ import nltk
 from re import sub
 from string import ascii_letters
 from weights import verified_weight, date_weight
-from utils import remove_none_alphabet, remove_stopwords, to_lower
+from utils import remove_non_alphabet, remove_stopwords, to_lower
 nltk.download('punkt')
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
+
+from similarity_algorithms.cosine import Cosine
+from similarity_algorithms.tfidf import Tfidf
 
 def similarity(filename, post_subject, post_text, N=3):
     threads = parse_file(filename)
@@ -27,19 +29,15 @@ def similarity(filename, post_subject, post_text, N=3):
     new_post = Post(None,post_subject,post_text,None)
     W = 0.2 # weight of subject similarity, payload weight is (1.0 - W)
 
-    filters = (  remove_none_alphabet, # take non-alphabetical words out
-                remove_stopwords)          # remove stopwords
+    filters = (remove_non_alphabet, remove_stopwords)
 
-    weights = [verified_weight, date_weight] #uses the functions from the weights.py script
+    weights = [verified_weight, date_weight]
     
-    cleaners =    ( to_lower,              # lowercase all text
-                    lambda x: sub(r'\s+', ' ', x))
+    cleaners = (to_lower, lambda x: sub(r'\s+', ' ', x))
 
     substitutes = tuple([])
 
-    from similarity_algorithms.tfidf import Tfidf
-
-    algorithms = (cosine_similarity, Tfidf().similarity)
+    algorithms = (Cosine().similarity, Tfidf().similarity)
 
     return [p for p in pipeline(new_post, threads, cleaners, filters, substitutes,  weights, algorithms, W, N)]
 
