ab97368803b2d9dead411eb4bbdff1ce5ac358f4
Allen Antony <22706998@student.uwa.edu.au>
2021-10-17 23:45:22 +0800

    Allows easier import of relevant functions

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .similarity import similar
+from .similarity_algorithms.cosine import Cosine
+from .similarity_algorithms.jaccard import Jaccard
+from .similarity_algorithms.use import Use
+from .similarity_algorithms.tfidf import Tfidf
