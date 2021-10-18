511422a36c78b9025a9f339b87acaaf071d244ca
Allen Antony <22706998@student.uwa.edu.au>
2021-10-18 15:49:08 +0800

    Bugfixing

--- install_USE.sh
+++ install_USE.sh
@@ -2,5 +2,5 @@
 
 wget -O  "universal-sentence-encoder_4.tar.gz" "https://tfhub.dev/google/universal-sentence-encoder/4?tf-hub-format=compressed"
 mkdir "universal-sentence-encoder_4"
-tar -xvzf "universal-sentence-encoder_4.tar.gz -C universal-sentence-encoder_4/"
+tar -xvzf "universal-sentence-encoder_4.tar.gz" -C "universal-sentence-encoder_4/"
 rm "universal-sentence-encoder_4.tar.gz"
