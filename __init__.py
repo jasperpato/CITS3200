ea3c6fb311fe1faebfca4840e5668937a637b65f
Allen Antony <22706998@student.uwa.edu.au>
2021-10-18 18:01:14 +0800

    Added clustering to __init__.py imports

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from .similarity_algorithms.cosine import Cosine
 from .similarity_algorithms.jaccard import Jaccard
 from .similarity_algorithms.use import Use
 from .similarity_algorithms.tfidf import Tfidf
+import clustering
